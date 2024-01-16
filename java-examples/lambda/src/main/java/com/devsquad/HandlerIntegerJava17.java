0fa81db25390637fb98460931ebedd6fdda4cfe9
Ana Maria Franco <anafranco@microsoft.com>
2024-01-16 11:08:58 -0500

    Adapting lambda to hello world

--- java-examples/lambda/src/main/java/com/devsquad/HandlerIntegerJava17.java
+++ java-examples/lambda/src/main/java/com/devsquad/HandlerIntegerJava17.java
@@ -5,20 +5,20 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 // Handler value: example.HandlerInteger
-public class HandlerIntegerJava17 implements RequestHandler<IntegerRecord, Integer>{
+public class HandlerIntegerJava17 implements RequestHandler<NameRecord, String>{
 
   @Override
   /*
    * Takes in an InputRecord, which contains two integers and a String.
    * Logs the String, then returns the sum of the two Integers.
    */
-  public Integer handleRequest(IntegerRecord event, Context context)
+  public String handleRequest(NameRecord event, Context context)
   {
     LambdaLogger logger = context.getLogger();
-    logger.log("String found: " + event.message());
-    return event.x() + event.y();
+    logger.log("String found: " + event.name());
+    return "Hello " + event.name() + "!";
   }
 }
 
-record IntegerRecord(int x, int y, String message) {
+record NameRecord(String name) {
 }
