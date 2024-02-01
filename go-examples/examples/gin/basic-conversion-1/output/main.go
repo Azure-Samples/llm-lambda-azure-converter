73f54861e08bc4c05441a91b9f3ca7d74e0fb5e1
Ana Maria Franco <anafranco@microsoft.com>
2024-02-01 15:14:22 -0500

    Fixing example output

--- go-examples/examples/gin/basic-conversion-1/output/main.go
+++ go-examples/examples/gin/basic-conversion-1/output/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,15 +19,15 @@ type MyResponse struct {
 	Message string `json:"message"`
 }
 
-func HandleRequest(ctx *gin.Context)  {
+func HandleRequest(ctx *gin.Context) {
 	if ctx.Request.Body == nil {
 		errorMsg := "received nil event"
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": errorMsg})
-		return 
+		return
 	}
 
 	var event MyEvent
-	err := ctx.Bind(&event)
+	err := ctx.ShouldBindJSON(&event)
 	if err != nil {
 		errorMsg := fmt.Sprintf("error on reading request body: %v\n", err.Error())
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": errorMsg})
@@ -44,11 +42,5 @@ func main() {
 	r := gin.Default()
 	r.Handle(http.MethodPost, "/HandleRequest", HandleRequest)
 
-	port := os.Getenv(EnvVarAzureFunctionPort)
-	if port == "" {
-		port = "8080"
-	}
-	host := fmt.Sprintf("0.0.0.0:%s", port)
-	fmt.Printf("Go server Listening...on port: %s\n", port)
-	log.Fatal(r.Run(host))
+	r.Run()
 }
